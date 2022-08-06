37174f5f9a7d2ef0ab59c8d43662ffc0cd6b6522
artor <artor.dev@gmail.com>
2022-08-06 14:21:39 +0200

    update progressbar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,17 +5,23 @@ const ProgressBar = (props) => {
     const { amount, total, msg_empty } = props
 
     // EXIT: no values
-    if(amount === 0 && total === 0) return <div>{msg_empty}</div>
+    if (amount === 0 && total === 0) return (
+        <div className='progressBar'>
+            <p>{msg_empty}</p>
+        </div >
+    )
 
     const percentage = amount * 100 / total
     const style = { width: percentage + "%" }
 
-    return (<div className='progressBar'>
-        <div>Progress: {amount} / {total} </div>
-        <div className='total'>
-            <div className='progress' style={style}></div>
+    return (
+        <div className='progressBar'>
+            <p>Progress: {amount} / {total} </p>
+            <div className='total'>
+                <div className='progress' style={style}></div>
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default ProgressBar
