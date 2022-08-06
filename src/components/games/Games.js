724bb32c3a6422f8768467ea95fb4cbea3e51cc9
artor <artor.dev@gmail.com>
2022-08-06 11:55:50 +0200

    cleanup games

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -12,90 +12,88 @@ import CharsIcon from '../../images/CharsIcon.svg';
 import ConnectIcon from '../../images/ConnectIcon.svg';
 import Header from '../layout/Header';
 import Footer from '../layout/Footer';
+import { getColorClassName } from '../../utils/className.js'
 
 const Games = () => {
-    let counter = 0;
     let counterBackground = 0;
-    let colourClass;
     let gamesBackground;
     let backgroundSize;
     let backgroundPosition;
     let gameName;
 
-    return(
-        <>
-        <div className='ContainerForHeaderAndMain'>
-            <Header />
-            <div className='mainContent'>
+    const games_data = [
+        {
+            name: 'Flascards',
+            style: {
+                backgroundImage: `url(${FlashcardsIcon})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'left 90% bottom 0%',
+                backgroundSize: '40%'
+            }
+        },
+        {
+            name: 'Memory',
+            style: {
+                backgroundImage: `url(${MemoryIcon})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'left 90% bottom 0%',
+                backgroundSize: '60%'
+            }
+        },
+        {
+            name: 'Multiple-Choice',
+            style: {
+                backgroundImage: `url(${MultipleChoiceIcon})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '90% 50%'
+            }
+        },
+        {
+            name: 'Chars',
+            style: {
+                backgroundImage: `url(${CharsIcon})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '90% 50%'
+            }
+        },
+        {
+            name: 'Connect',
+            style: {
+                backgroundImage: `url(${ConnectIcon})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '90% 50%',
+                backgroundSize: '40%'
+            }
+        },
 
-                {gamesArray.map((card) => {
-                    /*---- defining colour classNames ----*/
-                    counter < 4 ? counter++ : counter = 1;
-                    switch(counter) {
-                        case 1:
-                            colourClass = 'lightBlue';
-                            break;
-                        case 2:
-                            colourClass = 'darkBlue';
-                            break;
-                        case 3:
-                            colourClass = 'gray';
-                            break;
-                        case 4:
-                            colourClass = 'pink';
-                            break;
-                        default:
-                            colourClass = 'lightBlue';
-                    }
+    ]
 
-                    /*---- defining background sources ----*/
-                    counterBackground = counterBackground + 1;
-
-                    if(card.id === 1) {
-                        gameName = 'Flascards'
-                        gamesBackground = FlashcardsIcon;
-                        backgroundSize = '40%';
-                        backgroundPosition = 'left 90% bottom 0%';
-                    } else if (card.id === 2) {
-                        gameName = 'Memory'
-                        gamesBackground = MemoryIcon;
-                        backgroundSize = '60%';
-                        backgroundPosition = 'left 90% bottom 0%';
-                    } else if (card.id === 3) {
-                        gameName = 'Multiple-Choice'
-                        gamesBackground = MultipleChoiceIcon;
-                        backgroundSize = '';
-                        backgroundPosition = '90% 50%';
-                    } else if (card.id === 4) {
-                        gameName = 'Chars'
-                        gamesBackground = CharsIcon;
-                        backgroundSize = '';
-                        backgroundPosition = '90% 50%';
-                    } else if (card.id === 5) {
-                        gameName = 'Connect'
-                        gamesBackground = ConnectIcon;
-                        backgroundSize = '40%';
-                        backgroundPosition = '90% 50%';
-                    } else {
-                        console.log('error');
-                    }
-
-                    return(
-                        <div className={`decksForGames ${colourClass}`} key={card.id} >
-                            <NavLink to={'/games/' + gameName} className='decksNavLinkContainerGames' >
-                                <div className='cardGames' style={{backgroundImage: `url(${gamesBackground})`,  backgroundRepeat: 'no-repeat', backgroundPosition: backgroundPosition, backgroundSize: backgroundSize}} >
-                                    <h2>{card.name}</h2>
-                                </div>
-                            </NavLink>
-                            <div className='heartContainerGames' ><img src={card.liked ? filledHeart : emptyHeart} id={'heartOfCard' + card.id} alt="" /></div>
-                        </div>
-                    );
-                })
-                }
-                
+    const renderGames = () => !games_data.length
+        ? null
+        : (games_data.map((x, i) =>
+            <div className={`decksForGames ${getColorClassName(i)}`} key={i} >
+                <NavLink to={'/games/' + x.name} className='decksNavLinkContainerGames' >
+                    <div
+                        className='cardGames'
+                        style={x.style} >
+                        <h2>{x.name}</h2>
+                    </div>
+                </NavLink>
+                <div className='heartContainerGames' ><img src={x.liked ? filledHeart : emptyHeart} id={'heartOfCard' + i} alt="" /></div>
             </div>
-        </div>
-        <Footer />
+        ))
+
+    return (
+        <>
+            <div className='ContainerForHeaderAndMain'>
+                <Header />
+                <div className='mainContent'>
+
+                    {renderGames()}
+
+                </div>
+            </div>
+            <Footer />
         </>
     );
 }
