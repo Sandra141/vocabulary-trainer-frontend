e77fdc20cc9f76e98d2eb0bf30e238317cc385cb
Sandra Müller <muelsand20@gmail.com>
2022-08-08 14:36:55 +0200

    finishing up the memory Game

--- src/components/games/Flashcards.js
+++ src/components/games/Flashcards.js
@@ -3,8 +3,6 @@ import './../../css/flashcards.css';
 import { useVocabulary } from "../../contexts/Vocabulary";
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
-// import dummyDataArrayCards from "../../mockups/dummyDataArrayCards";
-import dummyDataArrayDecks from "../../mockups/dummyDataArrayDecks";
 
 const SHOWN_SIDE_ENUM = {
     front: "front",
@@ -14,12 +12,12 @@ const SHOWN_SIDE_ENUM = {
 const Flashcards = () => {
     const vocabulary = useVocabulary();
     const decks = vocabulary.decks;
-
     const [possibleDecks, setPossibleDeck] = useState([])
 
     const refPopupBackground = useRef(null);
     const [deckSelectionPopupIsShown, setDeckSelectionPopupIsShown] = useState(true);
     const [sideSelectionPopupIsShown, setSideSelectionPopupIsShown] = useState(false);
+    const [questionMarkPopupIsShown, setQuestionMarkPopupIsShown] = useState(false);
 
     const [deckSelection, setdeckSelection] = useState([]);
     const [shownSide, setShownSide] = useState(SHOWN_SIDE_ENUM.front);
@@ -41,26 +39,27 @@ const Flashcards = () => {
 
     useEffect(() => {
         if (!deckSelection.length) return
-
-        setSortedCards(deckSelection.sort((a, b) => a.ranking - b.ranking))
+        setSortedCards(deckSelection.sort((a, b) => a.rank - b.rank))
     }, [deckSelection])
 
     useEffect(() => {
         if (!deckSelection.length) return
+        console.log('resort')
 
         if(!tmpCards.length) {
             setTmpCards(sortedCards.slice(0, 7))
+            console.log('resort')
         }
     }, [sortedCards])
 
     useEffect(() => {
         const popupBackground = refPopupBackground.current;
-        if (deckSelectionPopupIsShown || sideSelectionPopupIsShown) {
+        if (deckSelectionPopupIsShown || sideSelectionPopupIsShown || questionMarkPopupIsShown) {
             popupBackground?.setAttribute('class', 'darkBackground');
         } else {
             popupBackground?.setAttribute('class', 'hidden');
         }
-    }, [deckSelectionPopupIsShown, sideSelectionPopupIsShown]);
+    }, [deckSelectionPopupIsShown, sideSelectionPopupIsShown, questionMarkPopupIsShown]);
 
     /*---- logic for selecting Decks ----*/
     const handleDeckSelection = (deckId) => {
@@ -86,44 +85,41 @@ const Flashcards = () => {
     /*---- game logic ----*/
     const processCard = (ranking) => {
         const cardsClone = [...tmpCards];
-
         const cardToUpdateIndex = cardsClone.findIndex(
             (c) => c._id === tmpCards[0]._id
         );
-
         cardsClone[cardToUpdateIndex].rank += ranking;
-
         cardsClone.sort((a, b) => a.rank - b.rank);
-
         // setSortedCards(cardsClone);
-
         const tmpCardsClone = [...tmpCards];
         tmpCardsClone.shift();
-
         console.log(6666666, tmpCardsClone)
-
         if (tmpCardsClone.length === 0) {
             console.log("repopulating buffer", cardsClone);
             // update
             vocabulary.updateCard(cardsClone)
-            setTmpCards(sortedCards.slice(0, 7));
+            setTmpCards(sortedCards.sort((a, b) => a.rank - b.rank).slice(0, 7));
         } else {
             setTmpCards(tmpCardsClone);
         }
     };
 
-    /*const handleCardTurn = () => {
-        setShownSide(shownSide === SHOWN_SIDE_ENUM.firstSide ? SHOWN_SIDE_ENUM.secondSide : SHOWN_SIDE_ENUM.firstSide);
-    }*/
-
-
     const renderDecks = () => {
-        const minNumberCards = 7
         const possibleDeck = decks
             .filter(x => vocabulary.getCardsFromDeckId(x._id).length >= 7)
 
         setPossibleDeck(possibleDeck)
+    }
 
+    /*---- question Mark logic ----*/
+    const handleQuestionMarkClick = () => {
+        setQuestionMarkPopupIsShown(true);
+    }
+
+    /*---- logic for closing the popup ----*/
+    const closePopup = (e) => {
+        document.body.style.overflow = 'visible';
+        setQuestionMarkPopupIsShown(false);
     }
 
     return (
@@ -133,7 +129,7 @@ const Flashcards = () => {
 
                 <div className='mainContent'>
 
-                    <div ref={refPopupBackground} ></div>
+                    <div ref={refPopupBackground} onClick={closePopup} ></div>
 
                     {/*--- popup container for deck selection ----*/}
                     <div className="popup" style={{ display: deckSelectionPopupIsShown ? 'block' : 'none' }} >
@@ -175,12 +171,23 @@ const Flashcards = () => {
                                 <div class="back">Solution:<br /><p>{tmpCards.length && tmpCards[0][hiddenSide]}</p></div>
                             </div>
                         </label>
-                        <div className="questionMark"></div>
+                        <div className="questionMark" onClick={handleQuestionMarkClick}>?</div>
                         <div className="difficultyContainer">
                             <div className="difficultyCard lightBlue" id='difficultyCardVeryEasy' onClick={() => processCard(3)} >Very Easy</div>
                             <div className="difficultyCard lightBlue" id='difficultyCardEasy' onClick={() => processCard(1)}>Easy</div>
                             <div className="difficultyCard lightBlue" id='difficultyCardVeryMedium' onClick={() => processCard(0)}>Medium</div>
                             <div className="difficultyCard lightBlue" id='difficultyCardVeryHard' onClick={() => processCard(-2)}>Hard</div>
+                        </div>
+                    </div>
+
+                    {/*--- popup container for game description ----*/}
+                    <div className="popup" style={{display: questionMarkPopupIsShown ? 'block' : 'none'}} >
+                        <div className='popupQuestionMark'>
+                            <h2 className='gamesPopupDeckSelectionH2'>How do flashcards work?</h2>
+                            <p className="gamesQuestionMarkP">Flashcards allow you to learn at your own pace. To start, you choose a deck and then you can choose the visible side.</p>
+                            <p className="gamesQuestionMarkP">As soon as the game starts, you will see the visible side of the card at the top. Your task is to find the solution. You can check your solution by clicking on the card.</p>
+                            <p className="gamesQuestionMarkP">Now you can assess how easy or difficult it was for you to find the solution and click on the corresponding button.</p>
+                            <p>Have fun!</p>
                         </div>
                     </div>
 
