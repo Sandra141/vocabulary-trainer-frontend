72bf541f4f9070b8477481e5b2a584cdd263e70b
artor <artor.dev@gmail.com>
2022-08-08 22:17:17 +0200

    mostly complete game

--- src/components/games/MultipleChoice.js
+++ src/components/games/MultipleChoice.js
@@ -1,96 +1,262 @@
 import React, { useRef, useState, useEffect } from "react";
+import './../../css/multipleChoice.css';
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
-import dummyDataArrayDecks from "../../mockups/dummyDataArrayDecks";
-import dummyDataArrayCards from "../../mockups/dummyDataArrayCards";
+import { useVocabulary } from "../../contexts/Vocabulary";
+
+const SHOWN_SIDE_ENUM = {
+    front: "front",
+    back: "back"
+}
+
+const POPUP_STATE = {
+    disable: "disable",
+    deck: "deck",
+    side: "side"
+}
+
+const timerFeedback = 1000
 
 const MultipleChoice = () => {
-    const refPopupBackground = useRef(null);
-    const [deckSelectionPopupIsShown, setDeckSelectionPopupIsShown] = useState(true);
-    const [sideSelectionPopupIsShown, setSideSelectionPopupIsShown] = useState(false);
-    const [deckSelection, setdeckSelection] = useState('');
-    const [hiddenSide, setHiddenSide] = useState('firstSide');
+    //# context
+    const vocabulary = useVocabulary();
 
-    /*---- logic for popup ----*/
-    const handleAddDecksButton = (e) => {
-        document.body.style.overflow = 'hidden';
-        setDeckSelectionPopupIsShown(current => !current);
+    //# variable
+    const decks = vocabulary.decks;
+    const minNumberCards = 7
+
+    // get decks with enough cards
+    const getPossibleDecks = () => decks.filter(x => vocabulary.getCardsFromDeckId(x._id).length >= minNumberCards)
+
+    //# state
+    const [possible_decks, set_possible_decks] = useState(getPossibleDecks())
+    const [selected_deck, set_selected_deck] = useState(null);
+    const [cards, set_cards] = useState([]);
+    const [tmpCards, setTmpCards] = useState([]);
+    const [show_card_question_side, set_show_card_question_side] = useState(SHOWN_SIDE_ENUM.back);
+    const [popup_state, set_popup_state] = useState(POPUP_STATE.deck);
+    const [possible_answers, set_possible_answers] = useState([])
+    const [show_feedback, set_show_feedback] = useState("")
+
+    //# logic
+    //## selecting Decks
+    const handleDeckSelection = deck_id => e => {
+        // set cards
+        const cardsFromDeck = vocabulary.getCardsFromDeckId(deck_id)
+        set_cards(cardsFromDeck)
+
+        // change popup state
+        set_popup_state(POPUP_STATE.side)
     }
 
-    useEffect(() => {
-        const popupBackground = refPopupBackground.current;
-        if (deckSelectionPopupIsShown || sideSelectionPopupIsShown) {
-            popupBackground?.setAttribute('class', 'darkBackground');
+    //## selecting hidden side
+    const handleSideSelection = side => e => {
+        // set
+        set_show_card_question_side(side)
+
+        // change popup state
+        set_popup_state(POPUP_STATE.disable)
+    }
+
+    //## get new rounds of cards
+    const getBadRankedCards = () => cards
+        // sort by rank
+        .sort((a, b) => a.rank - b.rank)
+        // max 7 cards
+        .slice(0, 7)
+        // shuffle
+        .sort(() => Math.random() - 0.5)
+
+    //## set one correct and one wrong solution to choose
+    const setSolutions = () => {
+        const right_solution_side = SHOWN_SIDE_ENUM.front ? tmpCards[0].back : tmpCards[0].front
+
+        const random_cards = cards.sort(() => Math.random() - 0.5)
+
+        // search for wrong solution
+        const wrong_solution_index = random_cards
+            .findIndex(x => {
+                const nase = SHOWN_SIDE_ENUM.front ? x.back : x.front
+
+                return nase !== right_solution_side
+            })
+
+        let wrong_solution
+        // EXIT: when no wrong solution found then return Kartoffel
+        if (wrong_solution_index === -1) {
+            wrong_solution = {
+                _id: tmpCards[0]._id,
+                name: "Kartoffel",
+                match: false
+            }
         } else {
-            popupBackground?.setAttribute('class', 'hidden');
+            const new_name = SHOWN_SIDE_ENUM.front ? random_cards[wrong_solution_index].back : random_cards[wrong_solution_index].front
+
+            wrong_solution = {
+                _id: tmpCards[0]._id,
+                name: new_name,
+                match: false
+            }
         }
-    }, [deckSelectionPopupIsShown, sideSelectionPopupIsShown]);
 
-    /*---- logic for selecting Decks ----*/
-    const handleDeckSelection = (e) => {
-        const deckId = e.target.id;
-        setdeckSelection(deckId);
-        setDeckSelectionPopupIsShown(false);
-        setSideSelectionPopupIsShown(true);
+        const new_right_solution = {
+            _id: tmpCards[0]._id,
+            name: right_solution_side,
+            match: true
+        }
+
+        const all_solutions = [new_right_solution, wrong_solution]
+
+        // shuffle and save
+        set_possible_answers(all_solutions.sort(() => Math.random() - 0.5))
     }
 
-    /*---- logic for selecting hidden side ----*/
-    const handleSideSelection = (e) => {
-        const clickedHiddenSide = e.target.id;
-        setHiddenSide(clickedHiddenSide);
-        setSideSelectionPopupIsShown(false);
+    //# user choose solution
+    const handleUserChooseSolution = solution => e => {
+        const find_index = cards.findIndex(x => x._id === solution._id)
+
+        if (find_index === -1) return
+
+        // is answer correct
+        if (solution.match) {
+            set_show_feedback("Correct =)")
+            cards[find_index].rank++
+        } else {
+            set_show_feedback("Wrong =(")
+            cards[find_index].rank--
+        }
+
+        vocabulary.updateCard(cards[find_index])
     }
 
+    //## new round
+    const startNewRound = () => {
+        const new_tmp_cards = getBadRankedCards()
+        setTmpCards(new_tmp_cards)
+    }
 
-    return (
-        <>
-            <div className='ContainerForHeaderAndMain'>
-                <Header />
-                <div className='mainContent'>
+    //# then
+    //## create tmp cards
+    useEffect(() => {
+        // EXIT: 
+        if (!cards.length) return
 
-                    <div ref={refPopupBackground} ></div>
+        startNewRound()
+    }, [cards])
 
-                    {/*--- popup container for deck selection ----*/}
-                    <div className="popup" style={{ display: deckSelectionPopupIsShown ? 'block' : 'none' }} >
-                        <div className='popupContentGames'>
-                            <h2 className='gamesPopupDeckSelectionH2'>Which Deck would you like to learn?</h2>
+    //## 
+    useEffect(() => {
+        if (!tmpCards.length) return
 
-                            {
-                                dummyDataArrayDecks.map((deck) => {
-                                    return (
-                                        <p onClick={handleDeckSelection} className="gamesPopupDeckSelectionP" id={deck.id} key={deck.id}>{deck.name}</p>
-                                    );
-                                })
-                            }
+        setSolutions()
+    }, [tmpCards])
+
+    //## disable feedback after time
+    useEffect(() => {
+        // EXIT: 
+        if (!show_feedback.length) return
+
+        const interval = setInterval(() => {
+            set_show_feedback("")
+            startNewRound()
+        }, timerFeedback)
+
+        return () => clearInterval(interval);
+    }, [show_feedback])
+
+    //# render
+    const renderFront = () => !tmpCards.length
+        ? null
+        : (
+            <div className="question">
+                {show_card_question_side === SHOWN_SIDE_ENUM.front ? tmpCards[0].front : tmpCards[0].back}
+            </div>
+        )
+
+    const renderPossibleSolutions = () => !tmpCards.length
+        ? null
+        : (
+            <div className="solution">
+                {possible_answers.map(x => <button className="lightBlue" onClick={handleUserChooseSolution(x)}>{x.name}</button>)}
+            </div>
+        )
+
+    const renderPossibleDecks = () => (
+        <div class='darkBackground'>
+            <div className="popup">
+                <div className='popupContentGames'>
+                    <h2 className='gamesPopupDeckSelectionH2'>Which Deck would you like to learn?</h2>
+
+                    {
+                        possible_decks.map((x, i) => <div onClick={handleDeckSelection(x._id)} id={i}>{x.name}</div>)
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+
+    const renderSelectCardSide = () => (
+        <div class='darkBackground'>
+            <div className="popup">
+                <div className='popupContentGames'>
+                    <h2 className='gamesPopupDeckSelectionH2'>Which side should be shown?</h2>
+
+                    <div className="selectSideContainer">
+                        <div className="sides" onClick={handleSideSelection(SHOWN_SIDE_ENUM.front)}>
+                            {cards[0]?.front}
                         </div>
-                    </div>
-
-                    {/*--- popup container for side selection ----*/}
-                    <div className="popup" style={{ display: sideSelectionPopupIsShown ? 'block' : 'none' }} >
-                        <div className='popupContentGames'>
-                            <h2 className='gamesPopupDeckSelectionH2'>Which side should be hidden?</h2>
-
-                            <div className="selectSideContainer">
-                                <div className="sides" id="firstSide" onClick={handleSideSelection}>
-                                    {dummyDataArrayCards.cards[0].front}
-                                </div>
-                                <div className="sides" id="secondSide" onClick={handleSideSelection}>
-                                    {dummyDataArrayCards.cards[0].back}
-                                </div>
-                            </div>
-
+                        <div className="sides" onClick={handleSideSelection(SHOWN_SIDE_ENUM.back)}>
+                            {cards[0]?.back}
                         </div>
-                    </div>
-
-                    {/*--- game setup ----*/}
-                    <div className="gameContainer">
-                        content
                     </div>
 
                 </div>
             </div>
+        </div>
+    )
+
+    const renderPopup = () => {
+        switch (popup_state) {
+            case POPUP_STATE.deck: return renderPossibleDecks()
+            case POPUP_STATE.side: return renderSelectCardSide()
+            case POPUP_STATE.disable: return null
+        }
+    }
+
+    const renderFeedback = () => !show_feedback
+        ? null
+        : <div className="feedback">
+            <div className="card">
+                <div>{show_feedback}</div>
+            </div>
+        </div>
+
+
+    return (
+        <div className='ContainerForHeaderAndMain'>
+
+            <Header />
+
+            <div className='mainContent multipleChoice'>
+
+                {renderPopup()}
+
+                {renderFeedback()}
+
+                <div className="layout">
+                    {renderFront()}
+
+                    <br/>
+
+                    {renderPossibleSolutions()}
+                </div>
+
+            </div>
+
             <Footer />
-        </>
+
+        </div>
     );
 }
 
