0a40d49bb84ecea264afbb3b9e5f4e81853bc584
Sandra Müller <muelsand20@gmail.com>
2022-08-09 11:50:42 +0200

    footer icons adjusted

--- src/components/layout/Footer2.js
+++ src/components/layout/Footer2.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './../../css/footer.css';
 import footerProfileGray from './../../images/footerProfileGray.svg';
-import footerHomeGray from './../../images/footerHomeGray.svg';
+import footerHomeWhite from './../../images/footerHomeWhite.svg';
 import footerGamesGray from './../../images/footerGamesGray.svg';
 
 const Footer = () => {
 
     return (
         <footer className="appFooter">
-            <NavLink to='/decks' id='home' className='active'><img  src={footerHomeGray} alt='home icon' /></NavLink>
+            <NavLink to='/decks' id='home' className='active'><img  src={footerHomeWhite} alt='home icon' /></NavLink>
             <NavLink to='/games' id='games'><img src={footerGamesGray} alt='games icon'/></NavLink>
             <NavLink to='/profile' id='profile' ><img src={footerProfileGray} alt='profile icon' /></NavLink>
         </footer>
